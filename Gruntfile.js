7886240182ee3dd2b9cbf7f6185f3be1f6d9ffe3
aZul FC <fmdcosta.piano@gmail.com>
2023-07-11 05:27:22 +0100

    Add grunt task to commit to screep server

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+    var config = require('./.screeps.json')
+    grunt.loadNpmTasks('grunt-screeps');
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: config.email,
+                token: config.token,
+                branch: config.branch,
+                ptr: config.ptr
+            },
+            dist: {
+                src: ['dist/*.js']
+            }
+        }
+    });
+}
