a06c094068ad92df9c604343f2ec4e412cdc0eb9
aZul FC <fmdcosta.piano@gmail.com>
2023-07-16 17:11:19 +0100

    Make a few config changes to grunt and tsc

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
                 ptr: config.ptr
             },
             dist: {
-                src: ['dist/*.js']
+                src: ['compiled/*.js']
             }
         }
     });
